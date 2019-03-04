1f5f7a1482507a019bc095381b3e870c3e9803bb
sseifert <sseifert@pro-vision.de>
2019-03-04 23:56:02 +0100

    Use wcm.io Link Handler Link Reference Container and Granite UI widgets for link selection. Use wcm.io Rich Text Handler RTE Custom Link plugin. Update test dependencies.

--- src/main/resources/archetype-resources/bundles/core/src/main/java/config/impl/LinkHandlerConfigImpl.java
+++ src/main/resources/archetype-resources/bundles/core/src/main/java/config/impl/LinkHandlerConfigImpl.java
@@ -3,12 +3,15 @@
 #set( $symbol_escape = '\' )
 package ${package}.config.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.osgi.service.component.annotations.Component;
 
 import com.day.cq.wcm.api.Page;
 
 import io.wcm.handler.link.spi.LinkHandlerConfig;
+import io.wcm.handler.link.type.MediaLinkType;
 import io.wcm.wcm.commons.util.Template;
 
 import ${package}.config.AppTemplate;
@@ -27,6 +30,14 @@ public class LinkHandlerConfigImpl extends LinkHandlerConfig {
   @Override
   public boolean isRedirect(@NotNull Page page) {
     return Template.is(page, AppTemplate.ADMIN_REDIRECT);
+  }
+
+  @Override
+  public @Nullable String getLinkRootPath(@NotNull Page page, @NotNull String linkTypeId) {
+    if (StringUtils.equals(linkTypeId, MediaLinkType.ID)) {
+      return "/content/dam/${projectName}";
+    }
+    return super.getLinkRootPath(page, linkTypeId);
   }
 
 }
