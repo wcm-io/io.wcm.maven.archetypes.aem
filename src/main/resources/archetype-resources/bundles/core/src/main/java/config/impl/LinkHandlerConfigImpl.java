f1f46f19289552ccc02f2a0f38f34b4c0295b4d9
sseifert <sseifert@pro-vision.de>
2020-07-04 11:56:08 +0200

    LinkHandler: Enable InternalCrossContextLinkType by default.
    (cherry picked from commit 54777fed8e1159276ea91ca427f852299e229ef5)

--- src/main/resources/archetype-resources/bundles/core/src/main/java/config/impl/LinkHandlerConfigImpl.java
+++ src/main/resources/archetype-resources/bundles/core/src/main/java/config/impl/LinkHandlerConfigImpl.java
@@ -3,14 +3,21 @@
 #set( $symbol_escape = '\' )
 package ${package}.config.impl;
 
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.osgi.service.component.annotations.Component;
 
 import com.day.cq.wcm.api.Page;
+import com.google.common.collect.ImmutableList;
 
 import io.wcm.handler.link.spi.LinkHandlerConfig;
+import io.wcm.handler.link.spi.LinkType;
+import io.wcm.handler.link.type.ExternalLinkType;
+import io.wcm.handler.link.type.InternalCrossContextLinkType;
+import io.wcm.handler.link.type.InternalLinkType;
 import io.wcm.handler.link.type.MediaLinkType;
 import io.wcm.wcm.commons.util.Template;
 
@@ -21,6 +28,17 @@ import ${package}.config.AppTemplate;
  */
 @Component(service = LinkHandlerConfig.class)
 public class LinkHandlerConfigImpl extends LinkHandlerConfig {
+
+  private static final List<Class<? extends LinkType>> DEFAULT_LINK_TYPES = ImmutableList.<Class<? extends LinkType>>of(
+      InternalLinkType.class,
+      InternalCrossContextLinkType.class,
+      ExternalLinkType.class,
+      MediaLinkType.class);
+
+  @Override
+  public @NotNull List<Class<? extends LinkType>> getLinkTypes() {
+    return DEFAULT_LINK_TYPES;
+  }
 
   @Override
   public boolean isValidLinkTarget(@NotNull Page page) {
