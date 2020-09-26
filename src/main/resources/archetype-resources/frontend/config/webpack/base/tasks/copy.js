13da92473f89017320ed9b1693b263bf29e7e03f
sseifert <sseifert@pro-vision.de>
2020-09-26 15:32:39 +0200

    update NPM dependencies

--- src/main/resources/archetype-resources/frontend/config/webpack/base/tasks/copy.js
+++ src/main/resources/archetype-resources/frontend/config/webpack/base/tasks/copy.js
@@ -4,11 +4,14 @@ import { pathConfig } from "../../../path.config";
 
 export const copy = {
   plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: `${path.resolve(pathConfig.paths.public)}/**/*.*`,
-        to: `${pathConfig.paths.public}`
-      }
-    ])
-  ]
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: `${path.resolve(pathConfig.paths.public)}/**/*.*`,
+          to: `${pathConfig.paths.public}`,
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
 };
