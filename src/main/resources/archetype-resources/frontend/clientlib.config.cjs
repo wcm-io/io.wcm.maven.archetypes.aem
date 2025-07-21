e053690a8573f415119ef318213544031814cd1e
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-07-21 23:31:30 +0800

    Frontend sample code: Update to Bootstrap 5 (#277)

--- src/main/resources/archetype-resources/frontend/clientlib.config.cjs
+++ src/main/resources/archetype-resources/frontend/clientlib.config.cjs
@@ -47,6 +47,7 @@ module.exports = {
         "${projectName}.app"
       ],
       jsProcessor: ["default:none", "min:gcc;compilationLevel=whitespace;languageIn=ECMASCRIPT_2018;languageOut=ECMASCRIPT_2018"],
+      cssProcessor: ["default:none", "min:none"],
 #if ( $optionAemVersion != "cloud" )
       longCacheKey: "${project.version}-${buildNumber}",
 #end
